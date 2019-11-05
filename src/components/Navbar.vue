5b5eb17ea180a5254d61ee464bc181cb67c0d2f5
Ato <jonathan.markin@unionsg.com>
2019-11-05 13:51:55 +0000

    some changes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,77 @@
+<template>
+    <nav>
+      <v-toolbar dark app>
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>PhotoClub</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+          <v-btn text>Link 1</v-btn>
+          <v-btn text>Link 2</v-btn>
+          <v-btn text>Link 3</v-btn>
+        </v-toolbar-items>
+
+        <template v-if="$vuetify.breakpoint.smAndUp">
+          <v-btn icon>
+            <v-icon>mdi-export-variant</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-delete-circle</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-plus-circle</v-icon>
+          </v-btn>
+        </template>
+     
+      </v-toolbar>
+
+      <v-navigation-drawer
+      class="deep-purple accent-4"
+      dark
+      permanent
+      v-model="drawer"
+      >
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block>Logout</v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+    </nav>
+
+    
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  components: {
+ 
+  },
+  data: () => ({
+    drawer:false,
+    items:[
+                {icon: 'dashboard', text: 'Dashboard', route: '/'},
+                {icon: 'folder', text: 'My Projects', route: '/projects'},
+                {icon: 'person', text: 'Team', route: '/team'}
+            ]
+  }),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <Navbar></Navbar>
+</div>
+  
+
+
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
