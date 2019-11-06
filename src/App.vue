ecd01c2c5ed46988116d60142ccddbdc90ec9bb0
Ato <jonathan.markin@unionsg.com>
2019-11-06 15:08:41 +0000

    Login halfway

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
- <v-app>
-  
-     <Home></Home>
- 
- 
- </v-app>
-  
+  <v-app >
+    <div class="about">
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-card app raised height="600" class="mt-5">
+            <v-layout row wrap > 
+              <v-flex md6 xs12>
+                <v-img
+                src="@/assets/images/antique-photo.jpg"
+                height="600"
+                >
+                </v-img>
+              </v-flex>
+              <v-flex md6 xs12 class="leftpane">
+                <v-container fluid>
+                  Ama
+                </v-container>               
+              </v-flex>
+            </v-layout>
+          </v-card>
+        </v-row>
+      </v-container>    
+    </div> 
+  </v-app> 
 </template>
 
 <script>
-import Home from '@/views/Home.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Home
-    
-  },
-  data: () => ({
-    //
-  }),
-};
+  
+}
 </script>
 
-<style lang="">
+<style scoped>
+
+@font-face {
+  font-family: GothamBook;
+  src: url("assets/fonts/Gotham-Font/GothamBook.ttf");
+}
+
+@font-face {
+  font-family: GothamMedium;
+  src: url("assets/fonts/Gotham-Font/GothamMedium.ttf");
+}
+
+.about{
+  /* The image used */
+  background-image: url("assets/images/antique-photo.jpg")!important;
+
+  /* Full height */
+  height: 100vh;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  font-family: GothamBook;
+}
+
+.leftpane{
+  background-color: #d1cbbb;
+  background-size: cover;
+}
   
-</style>
+</style>>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    Kwame
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+body, html {
+  height: 100%;
+}
+
+.about{
+  /* The image used */
+  background-image: url("../assets/images/antique-photo.jpg");
+
+  /* Full height */
+  height: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+  
+</style>>
+
+
